8243fe3db560360363e5ec554d2f62ed0d92cb16
tracer-ics2017 <tracer@njuics.org>
2023-03-17 19:48:50 +0800

    >  compile 20126792012679 wjm Linux wjm-VirtualBox 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  19:48:50 up  6:26,  1 user,  load average: 1.96, 1.58, 1.37 17c942377a6e87bb2f3be61a1765716970d53609

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -350,7 +350,7 @@ static bool make_token(char *e) {
         printf("change SUB to MUNIS\n");
         tokens[k].type = TK_MUNIS;
       }
-      if (k != 0 && tokens[k].type == TK_SUB && is_operator(tokens[k-1])){
+      if (k != 0 && tokens[k].type == TK_SUB && (is_operator(tokens[k-1])||tokens[k-1].type == TK_LP)){
         printf("change SUB to MUNIS\n");
         tokens[k].type = TK_MUNIS;
       }
@@ -359,7 +359,7 @@ static bool make_token(char *e) {
         printf("change MUL to POINTER\n");
         tokens[k].type = TK_POINTER;
       }
-      if(k != 0 && tokens[k].type == TK_MUL && is_operator(tokens[k-1])){
+      if(k != 0 && tokens[k].type == TK_MUL && (is_operator(tokens[k-1])||tokens[k-1].type == TK_LP)){
         printf("change MUL to POINTER\n");
         tokens[k].type = TK_POINTER;
       }
