6679ab0fe7711ce2ee115a518369023469609999
tracer-ics2017 <tracer@njuics.org>
2023-03-18 18:47:04 +0800

    >  compile 20126792012679 wjm Linux wjm-VirtualBox 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 i686 i686 GNU/Linux  18:47:04 up 20 min,  1 user,  load average: 2.51, 2.35, 1.81 66ccc940315303c2bfb33f0a6d5caa81f930560e

--- nemu/include/monitor/expr.h
+++ nemu/include/monitor/expr.h
@@ -2,7 +2,7 @@
 #define __EXPR_H__
 
 #include "common.h"
-void for_expr_test( char *);
+// void for_expr_test( char *);
 // 被声明为static的函数、变量或代码块只在当前文件中可见，不能被其他文件所访问。难怪在ui.c中用不了它/(ㄒoㄒ)/~~
 // static bool make_token(char *) ;
 uint32_t expr(char *, bool *);
