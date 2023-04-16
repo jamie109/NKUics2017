2987ff34c8138fdd35e485042644382894cb2210
jamie109 <2356408976@qq.com>
2023-04-16 16:15:23 +0800

    pa2

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -65,7 +65,6 @@ static inline void rtl_lm(rtlreg_t *dest, const rtlreg_t* addr, int len) {
 
 static inline void rtl_sm(rtlreg_t* addr, int len, const rtlreg_t* src1) {
   printf(">>start rtl_sm\n");
-
   vaddr_write(*addr, len, *src1);
   printf(">>finish rtl_sm\n");
 }
@@ -116,10 +115,10 @@ static inline void rtl_sr(int r, int width, const rtlreg_t* src1) {
 
 #define make_rtl_setget_eflags(f) \
   static inline void concat(rtl_set_, f) (const rtlreg_t* src) { \
-    TODO(); \
+    cpu.eflags.f = *src;\
   } \
   static inline void concat(rtl_get_, f) (rtlreg_t* dest) { \
-    TODO(); \
+    *dest = cpu.eflags.f;\
   }
 
 make_rtl_setget_eflags(CF)
@@ -129,17 +128,24 @@ make_rtl_setget_eflags(SF)
 
 static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
   // dest <- src1
-  TODO();
+  //TODO();
+  *dest = *src1;
 }
 
 static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
-  TODO();
+  //TODO();
+  *dest = ~(*dest);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  TODO();
+  //TODO();
+  // 需要进行移位的位数, reg t1
+  rtl_li(&t1, 32-width*8);
+  rtl_shl(dest, src1, &t1);
+  // 符号位
+  rtl_sar(dest, dest, &t1);
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
@@ -147,7 +153,7 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // M[esp] <- src1
   //TODO();
   // 栈顶
-  printf("start rfl_push for data\n");
+  printf("start rfl_push\n");
   cpu.esp = cpu.esp - 4;
   rtl_sm(&cpu.esp, 4, src1);
   printf("finish rfl_push\n");
@@ -156,37 +162,49 @@ static inline void rtl_push(const rtlreg_t* src1) {
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  TODO();
+  //TODO();
+  rtl_lm(dest, &cpu.esp, 4);
+  cpu.esp = cpu.esp + 4;
 }
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
-  TODO();
+  //TODO();
+  rtlreg_t tmp = *src1 == 0 ? 1 : 0;
+  rtl_lm(dest, &tmp, 4);
 }
 
 static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
   // dest <- (src1 == imm ? 1 : 0)
-  TODO();
+  //TODO();
+  rtlreg_t tmp = *src1 == imm ? 1 : 0;
+  rtl_lm(dest, &tmp, 4);
 }
 
 static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 != 0 ? 1 : 0)
-  TODO();
+  //TODO();
+  rtlreg_t tmp = *src1 != 0 ? 1 : 0;
+  rtl_lm(dest, &tmp, 4);
 }
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-  TODO();
+  //TODO();
+  rtl_shri(dest, src1, width*8-1);
 }
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-  TODO();
+  //TODO();
+  cpu.ZF=((*result)&(~0u>>((4-width)<<3)))==0;
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  TODO();
+  //TODO();
+  rtl_msb(&t0, result, width);
+  cpu.SF = t0;
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
