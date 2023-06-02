1c86279dab01c6f4a7035c560f0069d99fbc8cf4
tracer-ics2017 <tracer@njuics.org>
2023-06-02 18:17:13 +0800

    >  compile 20126792012679 wjm Linux wjm-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  18:17:13 up  3:58,  1 user,  load average: 0.68, 0.59, 0.44 3f57943c64762c788c8c48ccc06e845a99d27259

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,5 @@
 #include "nemu.h"
-
+#include "device/mmio.h"
 #define PMEM_SIZE (128 * 1024 * 1024)
 
 #define pmem_rw(addr, type) *(type *)({\
@@ -12,11 +12,23 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  //return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  //pa2 update
+  int r = is_mmio(addr);
+  if(r == -1)
+    return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  else
+    return mmio_read(addr,len,r);
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  //memcpy(guest_to_host(addr), &data, len);
+  //pa2 update
+  int r = is_mmio(addr);
+  if(r == -1)
+    memcpy(guest_to_host(addr), &data, len);
+  else
+    mmio_write(addr,len,data,r);
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
