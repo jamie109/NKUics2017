2efe8ecb0f683b8686c5843c3f6ff5578b45bf99
tracer-ics2017 <tracer@njuics.org>
2023-06-10 10:50:35 +0800

    >  compile 20126792012679 wjm Linux wjm-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  10:50:35 up 14 min,  1 user,  load average: 1.02, 1.07, 0.84 3d3ef5957f4f0901d444156defa88a35ebc497cd

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -59,7 +59,7 @@ paddr_t page_translate(vaddr_t addr, bool iswrite) {
       pte.dirty=1;
     }
     paddr_t paddr = PTE_ADDR(pte.val) | OFF(addr);
-    printf("vaddr=0x%x\tpaddr=0x%x\n", addr, paddr);
+    //printf("vaddr=0x%x\tpaddr=0x%x\n", addr, paddr);
     return paddr;
   }
 	return addr;
