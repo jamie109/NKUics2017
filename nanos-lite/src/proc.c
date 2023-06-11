eeba96e7b627e61dbb14c174ea5acacc43a02c22
tracer-ics2017 <tracer@njuics.org>
2023-06-11 15:33:53 +0800

    >  run 20126792012679 wjm Linux wjm-VirtualBox 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:33:53 up  3:52,  1 user,  load average: 2.60, 3.54, 2.43 1138ccfd64e64f18090377427222e42cffa4af2

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,15 +25,42 @@ void load_prog(const char *filename) {
 
   pcb[i].tf = _umake(&pcb[i].as, stack, stack, (void *)entry, NULL, NULL);
 }
-
+int current_game = 0;
+void switch_current_game() {
+  current_game = 2 - current_game;
+  Log("current_game = %d", current_game);
+}
 _RegSet* schedule(_RegSet *prev) {
-  //return NULL;
+  // //return NULL;
+  // if(current != NULL) {
+  //   current -> tf = prev;
+  // }
+  // //current = &pcb[0];
+  // current = (current == &pcb[0]? &pcb[1] : &pcb[0]);
+  // //Log("ptr = 0x%x\n", (uint32_t)current -> as.ptr); 
+  // _switch(&current -> as); 
+  // return current -> tf;
+  //pa4 优先级调度
   if(current != NULL) {
     current -> tf = prev;
   }
-  //current = &pcb[0];
-  current = (current == &pcb[0]? &pcb[1] : &pcb[0]);
-  //Log("ptr = 0x%x\n", (uint32_t)current -> as.ptr); 
-  _switch(&current -> as); 
+  else {
+    current = &pcb[current_game];
+  }
+  static int num = 0;
+  static const int frequency = 1000;
+  if(current == &pcb[current_game]) {
+    num++;
+  }
+  else {
+    current = &pcb[current_game];
+  }
+  if(num == frequency) {
+    current = &pcb[1];
+    num = 0;
+  }
+  // current = (current == &pcb[0]? &pcb[1] : &pcb[0]);
+  // Log("ptr = 0x%x\n", (uint32_t)current -> as.ptr);
+  _switch(&current -> as);
   return current -> tf;
 }
